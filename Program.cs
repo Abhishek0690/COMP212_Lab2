272572806d479587bb0d82e445341578736d5dad
Abhishek Narang <121088688+Abhishek0690@users.noreply.github.com>
2024-02-12 18:46:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _301271921_Abhishek__Lab3
+{
+    internal class Program
+    {
+        public LinkedList<int> reverseList(LinkedList<int> list)
+        {
+            LinkedList<int> newList = new LinkedList<int>();
+            foreach (var i in list)
+            {
+                newList.AddFirst(i);
+            }
+            return newList;
+        }
+        static void Main(string[] args)
+        { 
+            LinkedList<int> list = new LinkedList<int>();
+            //Entering 10 random integers to the list
+            Random random = new Random();
+            list.AddFirst(random.Next(100));
+            for(int i = 0; i <= 10; i++)
+            {
+                int j = random.Next(100);
+                list.AddAfter(list.First, j);
+            }
+            //Printing the List in Normal Order
+            foreach(int j in list)
+            {
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Reversing the List");
+            var reversedList = list.Reverse();
+            foreach (int j in reversedList)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
